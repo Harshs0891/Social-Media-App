3f2796b49a2160ffd427819f0f272e318620a98a
Harshs0891 <harshs0891@gmail.com>
2024-02-09 08:22:57 +0530

    added user registration form and setup for image upload

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +131,7 @@ LOGOUT_URL = 'logout'
 
 # Mailing system for reset passord
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#Store images
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm , UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def user_login(request):
@@ -24,3 +25,16 @@ def user_login(request):
 @login_required
 def index(request):
     return render(request,'users/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,'users/register_done.html')
+    else:
+        user_form=UserRegistrationForm()
+    return render(request,'users/register.html',{'user_form':user_form})
