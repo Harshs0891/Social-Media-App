fc619e0f72c41c5a2e1582ce877472d19960e895
Harshs0891 <harshs0891@gmail.com>
2024-02-12 09:41:32 +0530

    styled the home page

--- users/views.py
+++ users/views.py
@@ -32,7 +32,8 @@ def logout_user(request):
 def index(request):
     current_user = request.user
     posts=Post.objects.filter(user=current_user)
-    return render(request,'users/index.html',{'posts':posts})
+    profile=Profile.objects.filter(user=current_user).first()
+    return render(request,'users/index.html',{'posts':posts,'profile':profile})
 
 
 def register(request):
